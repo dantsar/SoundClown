22bff22d74a6077752028cb10522b973e1463978
dantsar <danieltsarev@gmail.com>
2024-02-05 21:41:22 -0500

    use java ffmpeg cli

--- Dockerfile
+++ Dockerfile
@@ -5,17 +5,19 @@ WORKDIR /code
 RUN set -x \
     && apt-get update \
     && apt-get install unzip \
-    && apt-get install --assume-yes git \
+    && apt-get install --assume-yes git ffmpeg \
     && VERSION=8.5 \
     && wget https://services.gradle.org/distributions/gradle-${VERSION}-bin.zip -P /tmp \
     && unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip \
     && ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest \
-    && rm /tmp/gradle-${VERSION}-bin.zip \
-    && wget https://downloads.apache.org/ant/binaries/apache-ant-1.10.13-bin.tar.gz -P /tmp \
-    && tar -xvzf /tmp/apache-ant-1.10.13-bin.tar.gz -C /opt \
-    && git clone https://JorenSix@github.com/JorenSix/TarsosTranscoder.git /opt/TarsosTranscoder \
-    && cd /opt/TarsosTranscoder/build \
-    && /opt/apache-ant-1.10.13/bin/ant
+    && rm /tmp/gradle-${VERSION}-bin.zip
+
+    # ABORTED THIS PROJECT
+    # && wget https://downloads.apache.org/ant/binaries/apache-ant-1.10.13-bin.tar.gz -P /tmp \
+    # && tar -xvzf /tmp/apache-ant-1.10.13-bin.tar.gz -C /opt \
+    # && git clone https://JorenSix@github.com/JorenSix/TarsosTranscoder.git /opt/TarsosTranscoder \
+    # && cd /opt/TarsosTranscoder/build \
+    # && /opt/apache-ant-1.10.13/bin/ant
 
 
 ENV GRADLE_HOME=/opt/gradle/latest/bin/
--- src/main/java/SoundClown/App.java
+++ src/main/java/SoundClown/App.java
@@ -1,7 +1,40 @@
 package SoundClown;
 
+import java.io.IOException;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.RunProcessFunction;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to SoundClown, the circus of sound!!!!!!!!!!!!!!!");
+
+        try {
+            FFmpeg ffmpeg = new FFmpeg("/usr/bin/ffmpeg");
+            FFprobe ffprobe = new FFprobe("/usr/bin/ffprobe");
+
+            FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput("/code/test_audio/audio_files/amen_beasty.wav")     // Filename, or a FFmpegProbeResult
+                .overrideOutputFiles(true) // Override the output if it exists
+                .addOutput("/code/test_audio/audio_files/BEST_AMEN_BREAK.mp3")   // Filename for the destination
+                    .setAudioCodec("libmp3lame") // using the LAME MP3 encoder
+                    .setAudioBitRate(128000) // Set the audio bitrate to 128 kbit/s
+                    .setAudioChannels(2) // Set the number of audio channels
+                    .setAudioSampleRate(44100) // Set the audio sample rate to 44.1 kHz
+                .done();
+
+            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+            // Run a one-pass encode
+            executor.createJob(builder).run();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
