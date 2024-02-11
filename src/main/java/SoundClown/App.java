cfa10097eb4224702c780a19f564d77692f22cae
ajaku <andyjaku13@gmail.com>
2024-02-10 21:53:15 -0500

    modify app to be act as server

--- src/main/java/SoundClown/App.java
+++ src/main/java/SoundClown/App.java
@@ -1,7 +1,11 @@
 package SoundClown;
 
+import SoundClown.webapp.Server;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to SoundClown, the circus of sound!!!!!!!!!!!!!!!");
+
+        Server server = new Server(5000);
     }
 }
