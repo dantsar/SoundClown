83f3b3fdf4915406fa4d1421d4b7a5f32e46c821
dantsar <danieltsarev@gmail.com>
2024-03-04 12:38:51 -0500

    rename AuidoUpload dir to AudioPlayer

--- src/main/java/com/SoundClown/AudioPlayer/AudioFileUploadController.java
+++ src/main/java/com/SoundClown/AudioPlayer/AudioFileUploadController.java
@@ -31,20 +31,39 @@ public class AudioFileUploadController {
 
 	@GetMapping("/allTracks")
 	public String listUploadedFiles(Model model) throws IOException {
-		System.out.println("REQUESTED TO LIST ALL TRACKS");
+		System.out.println("(listUploadedFiles) REQUESTED TO LIST ALL TRACKS");
 
+		// I'm going to need to modify this because serveFile refers to something else now...
+		// The problem is that I'm using Thymeleaf and it expects serveFile to map a HTTP request
 		model.addAttribute("files", storageService.loadAll().map(
 				path -> MvcUriComponentsBuilder.fromMethodName(AudioFileUploadController.class,
 						"serveFile", path.getFileName().toString()).build().toUri().toString())
 				.collect(Collectors.toList()));
 
-		return "uploadForm";
+		return "allTracks"; // this refers to allTracks.html in resources/templates
 	}
+
+	// Copilot output (to be deleted)
+	// public String playTrack(Model model) {
+	// 	System.out.println("REQUESTED TO PLAY TRACK");
+
+	// 	// Modify to refer to the right requesteTrack!
+	// 	model.addAttribute("requestdTrack", storageService.loadAll().map(
+	// 			path -> MvcUriComponentsBuilder.fromMethodName(AudioFileUploadController.class,
+	// 					"serveFile", path.getFileName().toString()).build().toUri().toString())
+	// 			.collect(Collectors.toList()));
+
+	// 	return "playTrack"; // this refers to playTrack.html in resources/templates
+	// }
+
+
+	// Note to self, it's failing because I don't have the @GetMapping("/track/{filename:.+}") annotation
+	// @GetMapping("/track/{filename:.+}")
 
 	@GetMapping("/track/{filename:.+}")
 	@ResponseBody
 	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-		System.out.println("REQUESTING TO DOWNLOAD TRACK: " + filename);
+		System.out.println("(serveFile) REQUESTING TO DOWNLOAD TRACK: " + filename);
 		Resource file = storageService.loadAsResource(filename);
 
 		if (file == null)
@@ -53,6 +72,36 @@ public class AudioFileUploadController {
 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
 				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
 	}
+
+	// @GetMapping("/track/{filename:.+}")
+	// @ResponseBody
+	// public String playTrack(@PathVariable String filename) {
+
+	// // public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+	// 	System.out.println("(playTrack) REQUESTING TO PLAY TRACK: " + filename);
+
+
+	// 	// // Add another html page to display and allow the track to be downloaded
+	// 	// System.out.println("REQUESTING TO DOWNLOAD TRACK: " + filename);
+	// 	// Resource file = storageService.loadAsResource(filename);
+
+	// 	// if (file == null) {
+	// 	// 	return ResponseEntity.notFound().build();
+	// 	// }
+
+	// 	// return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+	// 	// 		"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+
+	// 	// System.out.println("REQUESTED TO LIST ALL TRACKS");
+
+	// 	// // Modify to refer to the right requesteTrack!
+	// 	// model.addAttribute("requestdTrack", storageService.loadAll().map(
+	// 	// 		path -> MvcUriComponentsBuilder.fromMethodName(AudioFileUploadController.class,
+	// 	// 				"serveFile", path.getFileName().toString()).build().toUri().toString())
+	// 	// 		.collect(Collectors.toList()));
+
+	// 	return "playTrack"; // this refers to playTrack.html in resources/templates
+	// }
 
 	@PostMapping("/uploadTrack")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
