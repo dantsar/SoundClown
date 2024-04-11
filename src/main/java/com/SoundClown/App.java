98c32015980ffc5b3f7fa092e8b4a04c77a9b7ff
ajaku <andyjaku13@gmail.com>
2024-04-10 23:37:45 -0400

    add basic playlist api support to App.java

--- src/main/java/com/SoundClown/App.java
+++ src/main/java/com/SoundClown/App.java
@@ -31,50 +31,75 @@ public class App {
     @GetMapping("/get/user/{user_name}")
 	public ResponseEntity<?> getByUserName(@PathVariable("user_name") String user_name) {
 		UserAPI userAPI = new UserAPI();
-		return usersAPI.get_user(user_name, dcm);
+		return userAPI.get_user(user_name, dcm);
 	}
 
 	@PostMapping("/create/user")
 	public ResponseEntity<?> createNewUser(@RequestBody String json) {
 		UserAPI userAPI = new UserAPI();
-		return usersAPI.create_user(json, dcm);
+		return userAPI.create_user(json, dcm);
 	}
 
 	@PostMapping("/update/user")
 	public ResponseEntity<?> updateUser(@RequestBody String json) {
 		UserAPI userAPI = new UserAPI();
-		return usersAPI.update_user(json, dcm);
+		return userAPI.update_user(json, dcm);
 	}
 
 	@PostMapping("/delete/user/{user_name}")
 	public ResponseEntity<?> deleteUser(@PathVariable String user_name) {
 		UserAPI userAPI = new UserAPI();
-		return usersAPI.delete_user(user_name, dcm);
+		return userAPI.delete_user(user_name, dcm);
 	}
 
 	// Track
 	@GetMapping("/get/track/{track_name}")
 	public ResponseEntity<?> getByTrackName(@PathVariable("track_name") String track_name) {
 		TrackAPI trackAPI = new TrackAPI();
-		return tracksAPI.get_track(track_name, dcm);
+		return trackAPI.get_track(track_name, dcm);
 	}
 
 	@PostMapping("/create/track")
 	public ResponseEntity<?> createNewTrack(@RequestBody String json) throws JsonProcessingException {
 		TrackAPI trackAPI = new TrackAPI();
-		return tracksAPI.create_track(json, dcm);
+		return trackAPI.create_track(json, dcm);
 	}
 
 	@PostMapping("/update/track")
 	public ResponseEntity<?> updateTrack(@RequestBody String json) {
 		TrackAPI trackAPI = new TrackAPI();
-		return tracksAPI.update_track(json, dcm);
+		return trackAPI.update_track(json, dcm);
 	}
 
 	@PostMapping("/delete/track/{track_name}")
 	public ResponseEntity<?> deleteTrack(@PathVariable String track_name) {
 		TrackAPI trackAPI = new TrackAPI();
-		return tracksAPI.delete_track(track_name, dcm);
+		return trackAPI.delete_track(track_name, dcm);
+	}
+
+	// Playlist 
+	@GetMapping("/get/playlist/{playlist_name}")
+	public ResponseEntity<?> getByPlaylistName(@PathVariable("playlist_name") String track_name) {
+		PlaylistAPI playlistAPI = new PlaylistAPI();
+		return playlistAPI.get_playlist(track_name, dcm);
+	}
+
+	@PostMapping("/create/playlist")
+	public ResponseEntity<?> createNewPlaylist(@RequestBody String json) throws JsonProcessingException {
+		PlaylistAPI playlistAPI = new PlaylistAPI();
+		return playlistAPI.create_playlist(json, dcm);
+	}
+
+	@PostMapping("/update/playlist")
+	public ResponseEntity<?> updatePlaylist(@RequestBody String json) {
+		PlaylistAPI playlistAPI = new PlaylistAPI();
+		return playlistAPI.update_playlist(json, dcm);
+	}
+
+	@PostMapping("/delete/playlist/{track_name}")
+	public ResponseEntity<?> deletePlaylist(@PathVariable String playlist_name) {
+		PlaylistAPI playlistAPI = new PlaylistAPI();
+		return playlistAPI.delete_playlist(playlist_name, dcm);
 	}
 
 	public static void main(String[] args) {
