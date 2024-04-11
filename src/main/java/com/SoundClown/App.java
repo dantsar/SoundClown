942d221b0e437c0b845d180ed17a8ae1c09c5af5
ajaku <andyjaku13@gmail.com>
2024-04-10 21:16:37 -0400

    rename tracks instances to track

--- src/main/java/com/SoundClown/App.java
+++ src/main/java/com/SoundClown/App.java
@@ -29,50 +29,50 @@ public class App {
     // User
     @GetMapping("/get/user/{user_name}")
 	public ResponseEntity<?> getByUserName(@PathVariable("user_name") String user_name) {
-		UserAPI usersAPI = new UserAPI();
+		UserAPI userAPI = new UserAPI();
 		return usersAPI.get_user(user_name, dcm);
 	}
 
 	@PostMapping("/create/user")
 	public ResponseEntity<?> createNewUser(@RequestBody String json) {
-		UserAPI usersAPI = new UserAPI();
+		UserAPI userAPI = new UserAPI();
 		return usersAPI.create_user(json, dcm);
 	}
 
 	@PostMapping("/update/user")
 	public ResponseEntity<?> updateUser(@RequestBody String json) {
-		UserAPI usersAPI = new UserAPI();
+		UserAPI userAPI = new UserAPI();
 		return usersAPI.update_user(json, dcm);
 	}
 
 	@PostMapping("/delete/user/{user_name}")
 	public ResponseEntity<?> deleteUser(@PathVariable String user_name) {
-		UserAPI usersAPI = new UserAPI();
+		UserAPI userAPI = new UserAPI();
 		return usersAPI.delete_user(user_name, dcm);
 	}
 
 	// Track
 	@GetMapping("/get/track/{track_name}")
 	public ResponseEntity<?> getByTrackName(@PathVariable("track_name") String track_name) {
-		TrackAPI tracksAPI = new TrackAPI();
+		TrackAPI trackAPI = new TrackAPI();
 		return tracksAPI.get_track(track_name, dcm);
 	}
 
 	@PostMapping("/create/track")
 	public ResponseEntity<?> createNewTrack(@RequestBody String json) throws JsonProcessingException {
-		TrackAPI tracksAPI = new TrackAPI();
+		TrackAPI trackAPI = new TrackAPI();
 		return tracksAPI.create_track(json, dcm);
 	}
 
 	@PostMapping("/update/track")
 	public ResponseEntity<?> updateTrack(@RequestBody String json) {
-		TrackAPI tracksAPI = new TrackAPI();
+		TrackAPI trackAPI = new TrackAPI();
 		return tracksAPI.update_track(json, dcm);
 	}
 
 	@PostMapping("/delete/track/{track_name}")
 	public ResponseEntity<?> deleteTrack(@PathVariable String track_name) {
-		TrackAPI tracksAPI = new TrackAPI();
+		TrackAPI trackAPI = new TrackAPI();
 		return tracksAPI.delete_track(track_name, dcm);
 	}
 
