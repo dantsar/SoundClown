5c73410ec6a012bc073691f3a2bfb2b9c79fa639
ajaku <andyjaku13@gmail.com>
2024-04-12 00:06:33 -0400

    fix bug with UNIQUE in track table

--- create_table.sql
+++ create_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
 CREATE TABLE tracks (
     track_id    serial PRIMARY KEY,
     track_name  varchar(50)  NOT NULL,
-    artist_name varchar(50) UNIQUE NOT NULL,
+    artist_name varchar(50) NOT NULL,
     track_path  varchar(500) NOT NULL,
     plays       INT DEFAULT 0 NOT NULL,
     description varchar(500),
@@ -19,6 +19,6 @@ CREATE TABLE playlists (
     user_id       serial,
     track_id      serial,
     playlist_name varchar(500),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE,
     FOREIGN KEY (track_id) REFERENCES tracks(track_id)
 );
