c1030168c8e2136d3bbbbb27befc991b430b6537
fred13kim <fred13kim@gmail.com>
2024-04-14 12:13:56 -0400

    set CORS on controller

--- src/main/java/com/SoundClown/AudioPlayer/AudioFileUploadController.java
+++ src/main/java/com/SoundClown/AudioPlayer/AudioFileUploadController.java
@@ -24,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@CrossOrigin
 @Controller
 public class AudioFileUploadController {
 
