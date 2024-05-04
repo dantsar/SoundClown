09c9d0cda678ff8f79c26a54b55b25d8c102f628
ajaku <andyjaku13@gmail.com>
2024-05-04 17:57:37 -0400

    add login option if not already logged in

--- ui/src/WhoAmI.js
+++ ui/src/WhoAmI.js
@@ -6,24 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const WhoAmI = () => {
 
     const [user_name, setUsername] = useState(null);
-
-    // fetch('http://localhost:8080/whoami', {
-    //     method: 'GET',
-    //     headers: { "Accept": "text/plain" },
-    // }).then((response) => {
-    //     if (!response.ok) {
-    //         throw new Error('Network response was not ok ' + response.statusText);
-    //     }
-    //     console.log("response");
-    //     console.log(response);
-    //     return response.text();
-    // }).then((data) => {
-    //     console.log("data");
-    //     console.log(data);
-    //     setUsername(data.user_name);
-    // }).catch((error) => {
-    //     console.error('There has been a problem with your fetch operation:', error);
-    // });
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchUsername = async () => {
@@ -49,16 +32,23 @@ const WhoAmI = () => {
         fetchUsername();
       }, []); // Empty dependency array ensures this only runs once
 
+    const handleLoginClick = () => {
+        navigate('/login'); // Navigate to the login page
+    };
+
     if (user_name == "null") {
         return (
             <div className="whoAmI">
                 <p>Currently Not Logged In!</p>
+                <button onClick={handleLoginClick}>Login</button>
             </div>
         )
     } else {
         return (
             <div className="whoAmI">
                 <p>User: {user_name}</p>
+                <p>Want to login to another account?</p>
+                <button onClick={handleLoginClick}>Login</button>
             </div>
         );
     }
