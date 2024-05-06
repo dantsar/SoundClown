36e32a4fbf523ca790cbb7965ee6b16ce9df4dc3
ajaku <andyjaku13@gmail.com>
2024-05-06 02:50:08 -0400

    attempt to add tracks to playlist

--- ui/src/PlaylistDetails.js
+++ ui/src/PlaylistDetails.js
@@ -1,15 +1,18 @@
 import useFetch from './useFetch';
 import TrackList from './TrackList';
-import { useParams } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import User from './User';
 import Cookies from "js-cookie";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const PlaylistDetails = () => {
     const username = Cookies.get("username");
     const { playlist_id } = useParams();
     const [errorMsg, setErrorMsg] = useState("");
     const { data: playlist, error, isPending } = useFetch("http://localhost:8080/get/user/playlist/" + playlist_id);
+    const [tracks, setTracks] = useState([]);
+    const [trackName, setTrackName] = useState('');
+    const [tracksIsPending, setTracksIsPending] = useState(false);
 
     const handleAddTrack = async () => {
         if (username == null) {
@@ -25,7 +28,6 @@ const PlaylistDetails = () => {
                 credentials: 'include', // Include credentials for cookie support
                 body: JSON.stringify({
                     playlist_id: playlist_id,
-                    track_id: "42",
                 }),
             });
 
@@ -41,6 +43,39 @@ const PlaylistDetails = () => {
         window.location.reload();
     };
 
+    const searchClick = async () => {
+        console.log("Sending post");
+        try {
+            setTracksIsPending(true);
+            const response = await fetch('http://localhost:8080/get/tracks/track_name/' + trackName, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Add any additional headers if needed
+                },
+            });
+
+            // Handle the response
+            if (response.ok) {
+                const data = await response.json();
+                setTracks(data);
+            } else {
+                // Handle error response
+            }
+        } catch (error) {
+            // Handle network error or any other errors
+            console.error('Error:', error);
+        } finally {
+            setTracksIsPending(false);
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            searchClick();
+        }
+    };
+
      return (
         <div className='playlist-details'>
             {isPending && <div>Loading...</div>}
@@ -53,13 +88,42 @@ const PlaylistDetails = () => {
                             <p> Created by {playlist._user.username} </p>
                         </div>
                         {username === playlist._user.username && (
-                            <button onClick={handleAddTrack}>Add Track</button>
+                            <>
+                                <div className="Search" style={{marginBottom: '10px'}}>
+                                    <h2>Add a Track!</h2>
+                                </div>
+                                <div className="search-input-container">
+                                    <input
+                                        type="text"
+                                        value={trackName}
+                                        onChange={(e) => setTrackName(e.target.value)}
+                                        placeholder="Enter track name..."
+                                        className="search-input"
+                                        onKeyPress={handleKeyPress}
+                                    />
+                                    <button onClick={searchClick} className="search-button">Search</button>
+                                </div>
+                                <div>
+                                    <div className="track-list">
+                                        {tracks.map((track, index) => (
+                                            <>
+                                            <div className="track-preview" key={`${track._track_id}_${index}`}>
+                                                <h2>{track._track_name}</h2>
+                                                <div>
+                                                    Uploaded by <User user_id={track._artist_id}/>
+                                                </div>
+                                            </div>
+                                            <button onClick={handleAddTrack}>Add Track</button>
+                                            </>
+                                        ))}
+                                    </div>
+                                </div>
+                            </>
                         )}
-                        <TrackList tracks={playlist._tracks} title="" />
                     </div>
                 </>
             )}
         </div>
-    );
+     );
 };
 export default PlaylistDetails;
