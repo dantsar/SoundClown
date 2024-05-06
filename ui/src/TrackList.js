d1f2ad1c6e1ab652cd7deb5241b7b79beaa31ce2
fred13kim <fred13kim@gmail.com>
2024-05-06 07:51:28 -0400

    remove commented block

--- ui/src/TrackList.js
+++ ui/src/TrackList.js
@@ -2,53 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import User from './User';
 
-/* const TrackList = ({ tracks, title, onRemove, isEditing }) => {
-    const [trackToRemove, setTrackToRemove] = useState(null);
-    const [showRemoveButton, setShowRemoveButton] = useState(false);
-
-    const handleMore = (trackId) => {
-        // Toggle the visibility of the remove button when clicking the "..." button
-        setShowRemoveButton(!showRemoveButton);
-        // Set the track to remove
-        setTrackToRemove(trackId);
-    };
-
-    const handleRemove = () => {
-        // Call the onRemove function with the track to remove
-        onRemove(trackToRemove);
-        // Reset state
-        setTrackToRemove(null);
-        setShowRemoveButton(false);
-    };
-
-    return (
-        <div className='track-wrapper'>
-            <h2> {title} </h2>
-            <div className="track-list">
-                {tracks.map((track, index) => (
-                    <div className="track-preview" key={`${track._track_id}_${index}`}>
-                        <Link to={`/track/${track._track_id}`}>
-                            <h2>{track._track_name}</h2>
-                            <div>
-                                Uploaded by <User user_id={track._artist_id} />
-                            </div>
-                        </Link>
-                        {isEditing && (
-                            <div className="action-buttons">
-                                <button onClick={() => handleMore(track._track_id)}>...</button>
-                                {showRemoveButton && trackToRemove === track._track_id && (
-                                    <div className="confirmation-dialog">
-                                        <button onClick={handleRemove}>Remove from this playlist</button>
-                                    </div>
-                                )}
-                            </div>
-                        )}
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}; */
 const TrackList = ({ tracks, title, onRemove, isEditing }) => {
     const [trackToRemove, setTrackToRemove] = useState(null);
     const [showRemoveButtons, setShowRemoveButtons] = useState(Array(tracks.length).fill(false));
